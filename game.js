fc36fa548c16e555c3059ac5b41c0a975ac2b927
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-10 01:53:59 -0400

    Pokemon Clone: Bug working out scaling and position of multiple images

--- game.js
+++ game.js
@@ -8,35 +8,69 @@ function setCanvas() {
   canvas.height = window.innerHeight - 4;
 }
 
-setCanvas();
+function loadImages() {
+  // Declare html image objects
+  const bgImage = new Image();
+  const playerImage = new Image();
 
-function loadBG() {
-  // Images
-  const image = new Image();
-  image.src = './Assets/Images/Pellet Town.png';
-  image.onload = () => {
+  // Keeps pixel art sharp
+  context.imageSmoothingEnabled = false;
+
+  // Draw bg image
+  bgImage.onload = () => {
+    // Scaling constants
     const zoom = 2.5;
-    const scaleX = (canvas.width / image.width) * zoom;
-    const scaleY = (canvas.height / image.height) * zoom;
-    context.imageSmoothingEnabled = false; // Keeps pixel art sharp
+    const scaleX = canvas.width / bgImage.width;
+    const scaleY = canvas.height / bgImage.height;
+
     context.save();
-    context.scale(scaleX, scaleY);
-    context.drawImage(image, -200, -150);
+    context.scale(scaleX * zoom, scaleY * zoom);
+    context.drawImage(bgImage, -canvas.width / 10, -canvas.height / 8);
+    context.restore();
+
+    // Load player image
+    playerImage.src = './Assets/Images/playerDown.png';
+  };
+
+  // Draw player image
+  playerImage.onload = () => {
+    // Scaling constants
+    const zoom = 0.7;
+    const scaleX = canvas.width / bgImage.width;
+    const scaleY = canvas.height / bgImage.height;
+
+    // Draw player
+    context.save();
+    context.scale(scaleX * zoom, scaleY * zoom);
+    context.drawImage(
+      playerImage,
+      canvas.width / 2 - playerImage.width / 2,
+      canvas.height / 2 - playerImage.height / 2
+    );
     context.restore();
   };
+
+  // Set src / Begin loading images
+  bgImage.src = './Assets/Images/Pellet Town.png';
 }
 
-loadBG();
-
+// Event Handlers
 // Set window resizing event listener
 window.addEventListener('resize', function () {
+  // Clear context
+  context.clearRect(0, 0, canvas.width, canvas.height);
   // Set canvas dimensions
   setCanvas();
 
   // Reload images
-  loadBG();
+  loadImages();
 });
+
+// Initialization
+setCanvas();
 
 // Set background
 context.fillStyle = 'white';
 context.fillRect(0, 0, canvas.width, canvas.height);
+
+loadImages();
