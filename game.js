2c0b78ab594480fb7e6a712c0e07440bf158c5df
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-09 22:59:48 -0400

    Pokemon Clone: JS: Select and set canvas, get canvas context

---
+++ game.js
@@ -0,0 +1,8 @@
+// Selector and Context
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+
+// Set Canvas Dimensions
+canvas.width = 1024;
+canvas.height = 576;
+console.log(context);
