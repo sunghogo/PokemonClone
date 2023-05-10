48e04f89e2c2dcf2b4a5b702dd70cc031b2cb54c
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-05-10 00:03:31 -0400

    Pokemon Clone: JS: Load bg image and scales properly with window size

--- game.js
+++ game.js
@@ -2,7 +2,41 @@
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
-// Set Canvas Dimensions
-canvas.width = 1024;
-canvas.height = 576;
-console.log(context);
+function setCanvas() {
+  // Set canvas dimensions
+  canvas.width = window.innerWidth - 4;
+  canvas.height = window.innerHeight - 4;
+}
+
+setCanvas();
+
+function loadBG() {
+  // Images
+  const image = new Image();
+  image.src = './Assets/Images/Pellet Town.png';
+  console.log(canvas.height, canvas.width);
+  image.onload = () => {
+    const scaleX = (canvas.width / image.width) * 4;
+    const scaleY = (canvas.height / image.height) * 4;
+    console.log(scaleX, scaleY);
+    context.save();
+    context.scale(scaleX, scaleY);
+    context.drawImage(image, 0, 0);
+    context.restore();
+  };
+}
+
+loadBG();
+
+// Set window resizing event listener
+window.addEventListener('resize', function () {
+  // Set canvas dimensions
+  setCanvas();
+
+  // Reload images
+  loadBG();
+});
+
+// Set background
+context.fillStyle = 'white';
+context.fillRect(0, 0, canvas.width, canvas.height);
