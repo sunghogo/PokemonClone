b046de402e646dac7b05074bcec9d586b35833a6
sunghogo <leinad5592@gmail.com>
2023-05-14 10:28:32 -0400

    Pokemon Clone: generate-sources.js: Renamed naming convention on output files

--- scripts/generate-sources.js
+++ scripts/generate-sources.js
@@ -1,22 +1,20 @@
 // Declare fs, path, directories, file suffixes
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const directoryPaths = ["./images", "./audio"];
-const outputDirectory = "./data";
-const fileSuffix = "Sources.json";
+const directoryPaths = ['./images', './audio'];
+const outputDirectory = './data';
+const fileSuffix = '-sources.json';
 
 // Iterate over each src directory
-directoryPaths.forEach((directoryPath) =>
+directoryPaths.forEach(directoryPath =>
   fs.readdir(directoryPath, (err, files) => {
     // Catch 1st error
-    if (err) {
-      return console.log("Unable to scan directory: " + err);
-    }
+    if (err) return console.log(`${err}`);
 
     // Parse string paths into specified format
     const filePaths = [];
-    files.forEach((file) => {
+    files.forEach(file => {
       const filePath = `${directoryPath}/${file}`;
       filePaths.push(filePath);
     });
@@ -24,15 +22,12 @@ directoryPaths.forEach((directoryPath) =>
     // Write all the paths into a JSON file at specified output directory
     const outputFilePath = path.join(
       outputDirectory,
-      directoryPath.split("/").slice(-1) + fileSuffix
+      directoryPath.split('/').slice(-1) + fileSuffix
     );
-    fs.writeFile(outputFilePath, JSON.stringify(filePaths, null, 1), (err) => {
+    fs.writeFile(outputFilePath, JSON.stringify(filePaths, null, 1), err => {
       // Catch 2nd error
-      if (err) {
-        console.log(`Error writing ${outputFilePath}`, err);
-      } else {
-        console.log(`${outputFilePath} written successfully`);
-      }
+      if (err) return console.log(`${err}`);
+      else console.log(`${outputFilePath} written successfully`);
     });
   })
 );
