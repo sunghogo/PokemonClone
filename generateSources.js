77c566e1b4b74fb89968d586b5141b63a5fcc980
sunghogo <leinad5592@gmail.com>
2023-05-14 00:23:35 -0400

    Pokemon Clone: generateSources.js: Fixed file path, assigned expressions to variables for cleaner code, cleaned up debugging console.logs and made console.logs more descriptive

--- generateSources.js
+++ generateSources.js
@@ -23,25 +23,23 @@ directoryPaths.forEach((directoryPath) =>
     }
 
     // Parse string paths into specified format
-    let filePaths = [];
+    const filePaths = [];
     files.forEach((file) => {
-      filePaths.push(`${directoryPath}/${file}`);
+      const filePath = `${directoryPath}/${file}`;
+      filePaths.push(filePath);
     });
 
     // Write all the paths into a JSON file at specified output directory
-    fs.writeFile(
-      path.join(
-        outputDirectory,
-        directoryPath.split("/").slice(-1) + fileEnding
-      ),
-      JSON.stringify(filePaths, null, 1),
-      (err) => {
-        if (err) {
-          console.log("Error writing file:", err);
-        } else {
-          console.log("File written successfully");
-        }
-      }
+    const outputFilePath = path.join(
+      outputDirectory,
+      directoryPath.split("/").slice(-1) + fileEnding
     );
+    fs.writeFile(outputFilePath, JSON.stringify(filePaths, null, 1), (err) => {
+      if (err) {
+        console.log(`Error writing ${outputFilePath}`, err);
+      } else {
+        console.log(`${outputFilePath} written successfully`);
+      }
+    });
   })
 );
