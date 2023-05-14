1fd986bba3576d4de52ae825b885c5bed190afc5
sunghogo <leinad5592@gmail.com>
2023-05-14 00:23:11 -0400

    Pokemon Clone: generateSources.js: Fixed file path, assigned expressions to variables for cleaner code, cleaned up debugging console.logs and made console.logs more descriptive

--- generateSources.js
+++ generateSources.js
@@ -8,37 +8,38 @@
 //   console.log(files);
 // });
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const directoryPaths = ['./images', './audio'];
-const outputDirectory = './data';
+const directoryPaths = ["./images", "./audio"];
+const outputDirectory = "./data";
+const fileEnding = "Sources.json";
 
-directoryPaths.forEach(directoryPath =>
+directoryPaths.forEach((directoryPath) =>
   fs.readdir(directoryPath, (err, files) => {
     // Catch error
     if (err) {
-      return console.log('Unable to scan directory: ' + err);
+      return console.log("Unable to scan directory: " + err);
     }
 
+    // Parse string paths into specified format
     let filePaths = [];
-    files.forEach(file => {
-      filePaths.push(`.${directoryPath}/${file}`);
-      console.log(file);
+    files.forEach((file) => {
+      filePaths.push(`${directoryPath}/${file}`);
     });
-    console.log(filePaths);
 
+    // Write all the paths into a JSON file at specified output directory
     fs.writeFile(
       path.join(
         outputDirectory,
-        directoryPath.split('/').slice(-1) + 'Sources.json'
+        directoryPath.split("/").slice(-1) + fileEnding
       ),
       JSON.stringify(filePaths, null, 1),
-      err => {
+      (err) => {
         if (err) {
-          console.log('Error writing file:', err);
+          console.log("Error writing file:", err);
         } else {
-          console.log('File written successfully');
+          console.log("File written successfully");
         }
       }
     );
