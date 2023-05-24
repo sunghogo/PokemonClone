838daa3c66ee08c060bcdb1b8354189d002d0cc5
sunghogo <leinad5592@gmail.com>
2023-05-24 13:27:03 -0400

    Pokemon Clone: generate-sources.js: Refactored code into synchronous execution

--- src/generate-sources.js
+++ src/generate-sources.js
@@ -10,47 +10,30 @@ const sourcesPathsFileName = 'sources-paths.json';
 const sourcesPathsFilePath = path.join(outputDirectory, sourcesPathsFileName);
 const sourcesPaths = [];
 
-// Iterate over each src directory
-directoryPaths.forEach(directoryPath =>
-  fs.readdir(directoryPath, (err, files) => {
-    // Catch 1st error
-    if (err) return console.log(`${err}`);
+// Iterate over each asset directory
+directoryPaths.forEach(directoryPath => {
+  // Read files synchronously
+  const files = fs.readdirSync(directoryPath);
 
-    // Parse string paths into specified format
-    const filePaths = [];
-    files.forEach(file => {
-      const filePath = `${directoryPath}/${file}`; // src paths need to be in ./"" format
-      filePaths.push(filePath);
-    });
+  // Parse files names into specified format, and push them into array for writing
+  const filePaths = [];
+  files.forEach(file => {
+    const filePath = `${directoryPath}/${file}`; // src paths need to be in ./"" format
+    filePaths.push(filePath);
+  });
 
-    // Write all the paths into a JSON file at specified output directory, and then add this JSON file path to output file
-    const outputFilePath = path.join(
-      outputDirectory,
-      directoryPath.split('/').slice(-1) + fileSuffix
-    );
-    fs.writeFile(outputFilePath, JSON.stringify(filePaths, null, 1), err => {
-      // Catch 2nd error
-      if (err) return console.log(`${err}`);
-      else {
-        sourcesPaths.push(outputFilePath);
-        console.log(`${outputFilePath} written successfully`);
-      }
-    });
-  })
-);
+  // Generate output file name/path for each asset type, and push them into array for writing master source file
+  const outputFilePath = path.join(
+    outputDirectory,
+    directoryPath.split('/').slice(-1) + fileSuffix
+  );
+  sourcesPaths.push(outputFilePath);
 
-setTimeout(
-  _ =>
-    fs.writeFile(
-      sourcesPathsFilePath,
-      JSON.stringify(sourcesPaths, null, 1),
-      err => {
-        // Catch 2nd error
-        if (err) return console.log(`${err}`);
-        else {
-          console.log(`${sourcesPathsFilePath} written successfully`);
-        }
-      }
-    ),
-  500
-);
+  // Write all the file paths into a JSON file at specified output file path
+  fs.writeFileSync(outputFilePath, JSON.stringify(filePaths, null, 1));
+  console.log(`${outputFilePath} generated`);
+});
+
+// Write all the sources JSON file paths into a master JSON file
+fs.writeFileSync(sourcesPathsFilePath, JSON.stringify(sourcesPaths, null, 1));
+console.log(`${sourcesPathsFilePath} generated`);
